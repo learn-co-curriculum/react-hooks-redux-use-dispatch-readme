87399377b86599b22b159508af3876377827a631
Luke Ghenco <lukeghenco@gmail.com>
2017-02-14 13:40:08 -0500

    clean up starter code to be consistent with past lessons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import shoppingListItemReducer from './reducers/shoppingListItemReducer'
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
 const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
@@ -12,6 +12,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App store={store}/>
    </Provider>
-  ,
-  document.getElementById('root')
+  ,document.getElementById('root')
 );
